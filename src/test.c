af62888910ae66bd18b46c922efdac5e38416490
Wessam Elhefnawy <welhefna@cs.odu.edu>
2018-04-05 14:32:20 -0400

    add source code directory and files

---
+++ src/test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int my_strlen(char *s)
+{
+   char *p = s;
+
+   while (*p)
+      ++p;
+
+   return (p - s);
+}
+
+int main(void)
+{
+   int i;
+   char *s[] = 
+   {
+      "Git Test"
+   };
+
+   for (i = 0; i < 2; ++i)
+       printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+
+   return 0;
+}
